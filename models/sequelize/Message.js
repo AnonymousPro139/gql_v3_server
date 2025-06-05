59a1a50324e9f65c33272e20f9803296ed7a534f
AnonymousPro139 <anonymouspro139@gmail.com>
2025-06-05 11:49:48 -0700

    before add SSL certificate

--- models/sequelize/Message.js
+++ models/sequelize/Message.js
@@ -37,7 +37,7 @@ const Message = function (sequelize, DataTypes) {
         defaultValue: false,
       },
       fileViewToken: {
-        type: DataTypes.STRING(350),
+        type: DataTypes.STRING(320),
         defaultValue: "undefined",
       },
     },
